e5523747aec84a451eeb5e131dd517dc8be29089
Darrin Thompson <darrinth@gmail.com>
2009-12-09 23:31:33 -0500

    Hack together some "literate" docs to demonstrate basic use.
    In its current state basicuse.html isn't too attractive, but it's
    functional.

---
+++ doc/literate.js
@@ -0,0 +1,51 @@
+// A specialized listener which pokes results into a div with jquery.
+function LiterateListener(jqDest) {
+    this.jqDest = jqDest;
+}
+
+LiterateListener.prototype.noteResult = function(result) {
+    var status_string = result.status + ": " + result.name;
+    this.log(status_string);
+    this.logResult(result);
+    if (result.status == "pass") {
+	// do nothing.
+    } else {
+	this.jqDest.removeClass("green");
+	this.jqDest.addClass("red");
+    }
+    if (result.status == "fail") {
+        this.log("Failed case:");
+        this.log(result.failedCase.toString());
+    }
+};
+
+LiterateListener.prototype.done = function(result) {
+    this.log('done.');
+};
+
+LiterateListener.prototype.log = function(text) {
+    this.jqDest.append($('<span>' + text + '</span>'));
+    this.jqDest.append($('<br/>'));
+};
+
+LiterateListener.prototype.logResult = function(result) {
+    this.log("passes: " + result.counts.pass);
+    this.log("invalid: " + result.counts.invalid);
+}
+// Given a jquery identifier, run the code in the element and display
+// results in an helpful manner near the code.
+function literateRun(id) {
+    resetProps();
+    eval($(id).text());
+    var dest = $(id).after("<div>").next();
+    dest.addClass("test-result");
+    dest.addClass("green");
+    console.log(dest);
+    runAllProps(new Config(100, 1000), new LiterateListener(dest));
+}
+
+function runAllLiterate() {
+    $(".runme").each(function () {
+	    literateRun(this);
+	});
+}
